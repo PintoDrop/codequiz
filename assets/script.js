22006d895698b460d5d69f80487d707adfe9b020
PintoDrop <mikebender22@gmail.com>
2023-02-24 10:02:34 -0800

    testing some button features

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,11 @@
-document.getElementById("box1").addEventListener("click", changeColor);
-function changeColor() {
-  document.getElementById("box1").innerHTML = "CLICK!";
-  document.getElementById("box1").style.backgroundColor = "Green";
-}
+var beginQuiz = document.getElementById("begin");
+var button1 = document.getElementById("1")
+var button2 = document.getElementById("2");
+var button3 = document.getElementById("3");
+var button4 = document.getElementById("4");
+
+// document.getElementById("box1").addEventListener("click", changeColor);
+// function changeColor() {
+//   document.getElementById("box1").innerHTML = "CLICK!";
+//   document.getElementById("box1").style.backgroundColor = "Green";
+// }
