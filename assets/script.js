97fe6146bcc5df1f9e0374b8c26b014bacaeedb5
PintoDrop <mikebender22@gmail.com>
2023-02-26 12:22:38 -0800

    using button to trigger quiz

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,18 @@
-var beginQuiz = document.getElementById("begin");
 // var button1 = document.getElementById("1")
 // var button2 = document.getElementById("2");
 // var button3 = document.getElementById("3");
 // var button4 = document.getElementById("4");
+var beginBTN = document.getElementById("begin")
+var beginQuiz = document.getElementById("begin");
 
-function hideunhide(){
+const get=document.getElementById('begin');
+get.click();
+
+function clickEvent(){
+  console.log("Click Even triggered")
+  currentQuestion= 0;
   
-}
-
-var questions = [
+  let questions = [
   {
     prompt: "What does HTML stand for?\n (a) Hyperbolic Time Means Lazy\n (b) Hyper-Text Markup Language\n (c) Hiding Text Markup Lists",
     answer: "b"
@@ -17,10 +21,10 @@ var questions = [
     prompt: "What does DOM stand for?\n (a) Document Obsolete Margin\n (b) Danger Over Model\n (c) Document Object Model",
     answer: "c"
   } 
-]
-var score = 0;
-
-for(var i=0; i < questions.length; i++) {
+  ]
+  var score = 0;
+  
+  for(var i=0; i < questions.length; i++) {
   var response = window.prompt(questions[i].prompt);
   if(response == questions[i].answer){
     score++; 
@@ -28,18 +32,31 @@ for(var i=0; i < questions.length; i++) {
   } else {
     alert("Incorrect");
   }
+  }
+  
+  alert("You answered " + score + "/" + questions.length + " correctly");
 }
 
-alert("You got " + score + "/" + questions.length);
 
-// var question1 = document.getElementById ("q1");
+// var sec = 25;
+// var time = setInterval(myTimer, 1000);
 
-// document.getElementById("box1").addEventListener("click", changeColor);
-// function changeColor() {
-//   document.getElementById("box1").innerHTML = "CLICK!";
-//   document.getElementById("box1").style.backgroundColor = "Green";
+// function myTimer(){
+//   document.getElementById('timer').innerHTML = sec + ('sec left');
+//   sec--;
+//   if (sec == -1) {
+//     clearInterval(time);
+//     alert ('Times up!' : (''));
+//   } 
 // }
 
+// document.getElementById("begin").addEventListener("click", () => {
+//   startQuiz();
+//   var questions = startQuiz();
+//   document.getElementById("questions")
+// });
+
 document.getElementById("begin").addEventListener('click', event => {
+  // questions();
   document.getElementById("questions").classList.remove('hidden')
 })
