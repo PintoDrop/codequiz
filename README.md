1276342ea7b97301c2d4fce769c44e77d008b09c
PintoDrop <mikebender22@gmail.com>
2023-02-28 09:38:21 -0800

    tried new code, fixed timer a bit

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Description
 
-For this assignment I was tasked with creating a website from scratch that lets the user take a quiz to practice some knowledge about coding.
+For this assignment I was tasked with creating a website from scratch that lets the user take a quiz to practice some knowledge about coding. During this assignment I had to make a working quiz that used a timer to test the users knowledge of coding.
 
 ## Installation
 
@@ -10,7 +10,7 @@ N/A
 
 ## Usage
 
-This website is to help the user understand coding better and be able to test what they do know within the website.
+This website is to help the user understand coding better and be able to test what they do know within the website. The user is presented with questions and multiple choice answers. When the user gets a question right they are presented with an alert that it was correct and vice versa if they answered incorrectly.
 
 ## Credits
 
@@ -41,12 +41,12 @@ GIVEN I am taking a code quiz
 
 ## Repository Link
 
-[Github]( )
+[Github](https://github.com/PintoDrop/codequiz)
 
 ## Website Link
 
-[Password Generator]( )
+[Code-Quiz](https://pintodrop.github.io/codequiz/)
 
 ## Screen Shots
 
-![Deployed Website](./images/Deplyed-website.JPG)
+![Deployed Website](./assets/images/Website%20Screenshot.JPG)
--- assets/script.js
+++ assets/script.js
@@ -1,62 +1,140 @@
-// var button1 = document.getElementById("1")
-// var button2 = document.getElementById("2");
-// var button3 = document.getElementById("3");
-// var button4 = document.getElementById("4");
-// var beginBTN = document.getElementById("begin")
-// var beginQuiz = document.getElementById("begin");
-// get.click();
+var score = 0;
+var currentIndex = 0;
+var questionsDiv = document.getElementById("questions");
+var timer;
+var timerElement = document.getElementById("timer");
 
-const get=document.getElementById('begin');
-
-function clickEvent(){
-  console.log("Quiz Begins")
-  currentQuestion= 0;
-  
-  let questions = [
+var questions = [
   {
-    prompt: "What does HTML stand for?\n (a) Hyperbolic Time Means Lazy\n (b) Hyper-Text Markup Language\n (c) Hiding Text Markup Lists",
-    answer: "b"
+    prompt: "What does HTML stand for?",
+    choices: [
+      "Hyperbolic Time Means Lazy",
+      "Hyper-Text Markup Language",
+      "Hiding Text Markup Lists",
+    ],
+    answer: "Hyper-Text Markup Language",
+  },
+
+  {
+    prompt: "What does DOM stand for?",
+    choices: [
+      "Document Obsolete Margin",
+      "Danger Over Model",
+      "Document Object Model",
+    ],
+    answer: "Document Object Model",
   },
   {
-    prompt: "What does DOM stand for?\n (a) Document Obsolete Margin\n (b) Danger Over Model\n (c) Document Object Model",
-    answer: "c"
-  } 
-  ]
-  var score = 0;
-  
-  for(var i=0; i < questions.length; i++) {
-  var response = window.prompt(questions[i].prompt);
-  if(response == questions[i].answer){
-    score++; 
-    alert("Correct!");    
-  } else {
-    alert("Incorrect");
-  }
-  }
-  
-  alert("You answered " + score + "/" + questions.length + " correctly");
+    prompt: "What properties make up the box model?",
+    choices: [
+      "Margin, Padding, Border",
+      "Border, Font-Size, Flex",
+      "Function, Header, Float",
+    ],
+    answer: "Margin, Padding, Border",
+  },
+];
+var count = questions.length * 10;
+
+// function wingGame() {}
+
+// function loseGame() {}
+
+function initTimer() {
+  timer = setInterval(tick, 1000);
+  clearTimeout();
 }
 
+function tick() {
+  count--;
+  timerElement.textContent = count;
+  console.log(count);
+  if (count <= 0) {
+    // stop()
+    clearInterval(timer);
+    // clearTimeout();
+    return;
+  }
+}
 
-// var sec = 25;
-// var time = setInterval(myTimer, 1000);
+// call function to end quiz
+// function endQuiz() {
+//   clearInterval(myInterval);
+//   if (count <= 0) {
+//     clearTimeout();
+//   if (response == questions[i].answer) {
+//     score++;
+//     alert("Correct!");
+//   } else {
+//       alert("Incorrect");
 
-// function myTimer(){
-//   document.getElementById('timer').innerHTML = sec + ('sec left');
-//   sec--;
-//   if (sec == -1) {
-//     clearInterval(time);
-//     alert ('Times up!' : (''));
-//   } 
+//     }
 // }
 
-// document.getElementById("begin").addEventListener("click", () => {
-//   startQuiz();
-//   var questions = startQuiz();
-//   document.getElementById("questions")
-// });
+const get = document.getElementById("begin");
 
-document.getElementById("begin").addEventListener('click', event => {
-  // questions();
-  // document.getElementById("questions").classList.remove('hidden')
-})
+function renderQuestion() {
+  questionsDiv.innerHTML = ''
+  var currentQuestionObj = questions[currentIndex];
+  console.log(currentQuestionObj);
+  console.log("Quiz Begins");
+  currentQuestion = 0;
+  var promptEL = document.createElement("h1");
+  promptEL.textContent = currentQuestionObj.prompt;
+  questionsDiv.append(promptEL);
+
+  for (var i = 0; i < currentQuestionObj.choices.length; i++) {
+    var choice = currentQuestionObj.choices[i];
+    console.log(choice);
+    var choiceBtn = document.createElement("button");
+    choiceBtn.className = "choice";
+    choiceBtn.setAttribute("value", choice);
+    choiceBtn.textContent = `${i} ${choice}`;
+    questionsDiv.appendChild(choiceBtn);
+  }
+}
+
+function begin() {
+  initTimer();
+  renderQuestion();
+  // if (response == questions[i].answer) {
+  //   score++;
+  //   // alert("Correct!");
+  // } else {
+  //   alert("Incorrect");
+  // }
+  // alert("You answered " + score + "/" + questions.length + " correctly");
+}
+
+// }
+
+// function {}
+
+questionsDiv.addEventListener("click", function (event) {
+  if (event.target.className === "choice") {
+    // check if correct or not
+    currentIndex++
+    renderQuestion();
+  }
+});
+
+document.getElementById("begin").addEventListener("click", begin);
+endQuiz();
+
+// if (response == questions[i].answer) {
+//   score++;
+//   alert("Correct!");
+// } else {
+//   alert("Incorrect");
+// }
+
+// question: "What does HTML stand for?",
+// choiceA: "Hyperbolic Time Means Lazy",
+// choiceB: "Hyper-Text Markup Language",
+// choiceC: "Hiding Text Markup Lists",
+// correctAnswer: "b",
+
+//   {
+//   prompt: "How do you dance?\n (a) Document Obsolete Margin\n (b) Danger Over Model\n (c) Document Object Model",
+//   answer: "a"
+// },
